c1a001b8605c5e0ea941344b8246c5b160712e04
Tomoki-Fukasawa <74344215+Tomoki-Fukasawa@users.noreply.github.com>
2026-01-04 17:12:10 +0900

    1229user

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,4 +1,6 @@
 class BoardsController < ApplicationController
+  # before_action :move_to_index,except: [:index, :show]
+
   def index
     @boards = Board.all
   end
@@ -19,4 +21,10 @@ class BoardsController < ApplicationController
   def board_params
     params.require(:board).permit(:title,:description).merge(user_id: current_user.id)
   end
+
+  # def move_to_index
+  #   unless user_signed_in?
+  #     redirect_to action: :index
+  #   end
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root to: 'boards#index'
   resources :boards, only: [:index,:new,:create]
 end
