ab5f10814b7f3c8679c7d5aa39999749d632aecb
Tomoki-Fukasawa <74344215+Tomoki-Fukasawa@users.noreply.github.com>
2026-02-08 16:44:48 +0900

    0208_test

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,6 +1,7 @@
 class BoardsController < ApplicationController
+  before_action :set_board, only: [:show,:edit,:update,:destroy]
   before_action :move_to_index,except: [:index, :show]
-  before_action :authenticated_root_path!, only: [:edit, :update, :destroy]
+  before_action :authenticated_board!, only: [:edit, :update, :destroy]
 
   def index
     @boards = Board.all
@@ -9,9 +10,9 @@ class BoardsController < ApplicationController
     @board=Board.new
   end
   def create
-    @board=Board.create(board_params)
+    @board=current_user.boards.new(board_params)
     if @board.save
-      redirect_to '/'
+      redirect_to authenticated_root_path
     else
       render :new, status: :unprocessable_entity
     end
@@ -19,30 +20,30 @@ class BoardsController < ApplicationController
 
   def show
     @comment=Comment.new   
-    @board=Board.find(params[:id]) 
     @comments=@board.comments.includes(:user)
   end
 
   def destroy
-    board=Board.find(params[:id])
-    board.destroy
+    @board.destroy
     redirect_to '/'
   end
 
   def edit
-    @board = Board.find(params[:id])
   end
 
   def update
-    board = Board.find(params[:id])
-    board.update(board_params)
+    @board.update(board_params)
     redirect_to '/'
   end
 
   private
 
   def board_params
-    params.require(:board).permit(:title,:description).merge(user_id: current_user.id)
+    params.require(:board).permit(:title,:description)
+  end
+
+  def set_board
+    @board = Board.find(params[:id])
   end
 
   def move_to_index
@@ -51,7 +52,7 @@ class BoardsController < ApplicationController
     end
   end
 
-  def authorize_board!
+  def authenticated_board!
     redirect_to authenticated_root_path unless @board.user == current_user
   end
 end
--- spec/system/boards_spec.rb
+++ spec/system/boards_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'ツイート投稿', type: :system do
+RSpec.describe '掲示板投稿', type: :system do
   before do
     @user = FactoryBot.create(:user)
     @board_title = Faker::Lorem.sentence
@@ -10,22 +10,25 @@ RSpec.describe 'ツイート投稿', type: :system do
     it 'ログインしたユーザーは新規投稿できる' do
       # ログインする
       visit new_user_session_path
-      fill_in 'Email', with:@user.email
-      fill_in 'Password', with:@user.password
+      fill_in 'メールアドレス', with:@user.email
+      fill_in 'パスワード', with:@user.password
       find('input[name="commit"]').click
       expect(page).to have_current_path(authenticated_root_path)
       # 新規投稿ページへのボタンがあることを確認する
       expect(page).to have_link('掲示板・ブログを立てる', href: new_board_path)
       # 投稿ページに移動する
       visit new_board_path
+      expect(page).to have_content('新規投稿ページ')
       # フォームに情報を入力する
-      fill_in 'タイトル', with:@board_title
-      fill_in '内容', with:@board_description
-      # 送信するとTweetモデルのカウントが1上がることを確認する
-      expect{
-        find('input[name="commit"]')click
-        sleep 1
-      }.to change{Board.count}.by(1) 
+      fill_in '投稿タイトル', with:@board_title
+      fill_in '投稿内容', with:@board_description
+      # 送信するとBoardモデルのカウントが1上がることを確認する
+      # expect{
+        # find('input[name="commit"]').click
+        click_button '掲示板を公開する'
+        expect(page).to have_content(@board_title)
+        expect(page).to have_content(@board_description)
+      # }.to change{Board.count}.by(1) 
       # トップページには先ほど投稿した内容のツイートが存在することを確認する（画像）
       expect(page).to have_content(@board_title)
       # トップページには先ほど投稿した内容のツイートが存在することを確認する（テキスト）
@@ -43,162 +46,191 @@ RSpec.describe 'ツイート投稿', type: :system do
 end
 
 
-RSpec.describe 'ツイート編集', type: :system do
+RSpec.describe '掲示板編集', type: :system do
   before do
-    @board1 = FactoryBot.create(:board)
+    @user = FactoryBot.create(:user)
+    @board1 = FactoryBot.create(:board, user: @user)
     @board2 = FactoryBot.create(:board)
   end
-  context 'ツイート編集ができるとき' do
+  context '掲示板編集ができるとき' do
     it 'ログインしたユーザーは自分が投稿したツイートの編集ができる' do
-      # ツイート1を投稿したユーザーでログインする
+      # 掲示板1を投稿したユーザーでログインする
       visit new_user_session_path
-      fill_in 'Email',with:@board1.user.email
-      fill_in 'Password',with:@board1.user.password
+      fill_in 'メールアドレス',with:@user.email
+      fill_in 'パスワード',with:@user.password
       find('input[name="commit"]').click
       expect(page).to have_current_path(authenticated_root_path)
-      # ツイート1に「編集」へのリンクがあることを確認する
-      expect(
-        all('.more')[1].hover
-      ).to have_link'edit',href: edit_board_path(@board1)
-      # 編集ページへ遷移する
-      visit edit_tweet_path(@board1)
+      # 掲示板1に「'この掲示板についてはこちら'」へのリンクがあることを確認する
+      within all('.board-list')[0]do
+        expect(page).to have_link 'この掲示板についてはこちら'
+      end
+      # 詳細ページへ遷移する
+      visit board_path(@board1)
+      # 掲示板編集のリンクがあることを確認・移動
+      within '.more' do
+        click_link '編集'
+      end
+      #掲示板編集の画面に移動後、
       # すでに投稿済みの内容がフォームに入っていることを確認する
-      expect(page).to have_field('tweet_image', with:@board1.title)
-      expect(page).to have_field('tweet_text', with:@board1.description)
+      expect(page).to have_current_path(edit_board_path(@board1))      
+      expect(page).to have_field('投稿タイトル', with:@board1.title)
+      expect(page).to have_field('投稿内容', with:@board1.description)
       # 投稿内容を編集する
-      fill_in 'タイトル',with: "#{@board1.title}+gazourl"
-      fill_in '内容',with: "#{@board1.description}+text"
-      # 編集してもTweetモデルのカウントは変わらないことを確認する
+      fill_in '投稿タイトル',with: "#{@board1.title}+編集したtitle"
+      fill_in '投稿内容',with: "#{@board1.description}+編集したdescription"
+      # 編集してもBoardモデルのカウントは変わらないことを確認する
       expect{
-        find('input[name="commit"]').click
-        sleep 1
+        # find('input[name="commit"]').click
+        click_button '掲示板を更新する'
       }.to change{Board.count}.by(0)
-      # トップページには先ほど変更した内容のツイートが存在することを確認する（画像）
-      expect(page).to have_selector ".content_post[style='background-image: url(#{@board1.title}+編集した画像URL);']"
-      # トップページには先ほど変更した内容のツイートが存在することを確認する（テキスト）
-      expect(page).to have_content("#{@board1.description}+編集したテキスト")
+      # トップページには先ほど変更した内容の掲示板が存在することを確認する（タイトル）
+      # expect(page).to have_selector ".content_post[style='background-image: url(#{@board1.title}+編集した画像URL);']"
+      expect(page).to have_content("#{@board1.title}+編集したtitle")
+      # トップページには先ほど変更した内容のツイートが存在することを確認する（説明文）
+      expect(page).to have_content("#{@board1.description}+編集したdescription")
     end
   end
-  context 'ツイート編集ができないとき' do
-    it 'ログインしたユーザーは自分以外が投稿したツイートの編集画面には遷移できない' do
-      # ツイート1を投稿したユーザーでログインする
+  context '掲示板編集ができないとき' do
+    it 'ログインしたユーザーは自分以外が投稿した掲示板の編集画面には遷移できない' do
+      # 掲示板1を投稿したユーザーでログインする
       visit new_user_session_path
-      fill_in 'Email', with:@board1.user.email
-      fill_in 'Password' with:@board1.user.password
+      fill_in 'メールアドレス', with:@user.email
+      fill_in 'パスワード', with:@user.password
       find('input[name="commit"]').click
       expect(page).to have_current_path(authenticated_root_path)
-      # ツイート2に「編集」へのリンクがないことを確認する
-      expect(
-        all(.more)[0].hover
-      ).to have_no_link 'edit',href: edit_tweet_path(@board2)
-
+      # 詳細ページへ遷移する
+      visit board_path(@board2)
+      # 掲示板2に「'編集'」へのリンクがないことを確認する
+      # within all('.board-list')[1]do
+      #   expect(page).to have_no_link 'この掲示板についてはこちら'
+      # end
+      within '.more' do
+        expect(page).to have_no_link '編集', href:edit_board_path(@board1)
+      end
     end
-    it 'ログインしていないとツイートの編集画面には遷移できない' do
+    it 'ログインしていないと掲示板の編集画面には遷移できない' do
       # トップページにいる
       visit unauthenticated_root_path
-      # ツイート1に「編集」へのリンクがないことを確認する
-      expect(
-        all('.more')[1].hover
-      ).to have_no_link 'edit', href: edit_board_path(@board1)
-      # ツイート2に「編集」へのリンクがないことを確認する
-      expect(
-        all('.more')[2].hover
-      ).to have_no_link 'edit', href: edit_board_path(@board2)
+      # ツイート1に「'編集'」へのリンクがないことを確認する
+      visit board_path(@board1)
+      within '.more' do
+        expect(page).to have_no_link '編集', href:edit_board_path(@board1)
+      end
+      # ツイート2に「'編集'」へのリンクがないことを確認する
+      visit board_path(@board2)
+      within '.more' do
+        expect(page).to have_no_link '編集', href:edit_board_path(@board1)
+      end
     end
   end
 end
 
-RSpec.describe 'ツイート削除', type: :system do
+RSpec.describe '掲示板削除', type: :system do
   before do
-    @board1 = FactoryBot.create(:board)
+    @user = FactoryBot.create(:user)
+    @board1 = FactoryBot.create(:board, user: @user)
     @board2 = FactoryBot.create(:board)
   end
-  context 'ツイート削除ができるとき' do
-    it 'ログインしたユーザーは自らが投稿したツイートの削除ができる' do
+  context '掲示板削除ができるとき' do
+    it 'ログインしたユーザーは自らが投稿した掲示板の削除ができる' do
       # ツイート1を投稿したユーザーでログインする
       visit new_user_session_path
-      fill_in 'Email', with:@board.user.email
-      fill_in 'Password', with:@board.user.password
+      fill_in 'メールアドレス', with:@user.email
+      fill_in 'パスワード', with:@user.password
       find('input[name="commit"]').click
       expect(page).to have_current_path(authenticated_root_path)
-      # ツイート1に「削除」へのリンクがあることを確認する
-      expect(
-        all('.more')[1].hover
-      ).to have_link 'destroy', href:board_path(@board1)
+      # ツイート1に「'この掲示板についてはこちら'」へのリンクがあることを確認する
+      within all('.board-list')[0]do
+        expect(page).to have_link 'この掲示板についてはこちら'
+      end
+      # 掲示板詳細画面へ移動
+      visit board_path(@board1)
+      # 掲示板削除のリンクがあることを確認
+      # expect(
+      #   all('.more')[1].hover
+      # ).to have_link 'destroy', href:board_path(@board1)
+      within '.more' do
+        expect(page).to have_link '削除'
+      end
       # 投稿を削除するとレコードの数が1減ることを確認する
       expect{
-        all('.more')[1].hover.find_link('destroy',href: board_path(@board1)).click
-        sleep 1
+        # all('.more')[0].find_link('削除',href: board_path(@board1)).click
+        click_link '削除'
       }.to change{Board.count}.by(-1)
-      # トップページにはツイート1の内容が存在しないことを確認する（画像）
-      expect(page).to have_no_selector ".content_post[style='background-image: url(#{@board1.title});']"
-      # トップページにはツイート1の内容が存在しないことを確認する（テキスト）
+      # トップページには掲示板1の内容が存在しないことを確認する（タイトル）
+      expect(page).to have_no_content("#{@board1.title}")
+      # トップページには掲示板1の内容が存在しないことを確認する（説明文）
       expect(page).to have_no_content("#{@board1.description}")
     end
   end
-  context 'ツイート削除ができないとき' do
-    it 'ログインしたユーザーは自分以外が投稿したツイートの削除ができない' do
-      # ツイート1を投稿したユーザーでログインする
+  context '掲示板削除ができないとき' do
+    it 'ログインしたユーザーは自分以外が投稿した掲示板の削除ができない' do
+      # 掲示板1を投稿したユーザーでログインする
       visit new_user_session_path
-      fill_in 'Email', with: @board1.user.email
-      fill_in 'Password', with: @board1.user.password
+      fill_in 'メールアドレス', with: @user.email
+      fill_in 'パスワード', with: @user.password
       find('input[name="commit"]').click
       expect(page).to have_current_path(authenticated_root_path)
-      # ツイート2に「削除」へのリンクがないことを確認する
-      expect(
-        all('.more')[0].hover
-      ).to have_no_link '削除', href: board_path(@board2)
+      # 掲示板2に「'削除'」へのリンクがないことを確認する
+      visit board_path(@board2)
+      within '.more' do
+        expect(page).to have_no_link '削除'
+      end
     end
-    it 'ログインしていないとツイートの削除ボタンがない' do
+    it 'ログインしていないと掲示板の削除ボタンがない' do
       # トップページに移動する
       visit unauthenticated_root_path
-      # ツイート1に「削除」へのリンクがないことを確認する
-      expect(
-        all('.more')[1].hover
-      ).to have_no_link 'destroy', href:board_path(@board1)
-      # ツイート2に「削除」へのリンクがないことを確認する
-      expect(
-        all('.more')[0].hover
-      ).to have_no_link 'destroy', href:board_path(@board2)
+      # ツイート1に「'削除'」へのリンクがないことを確認する
+      visit board_path(@board1)
+      within '.more' do
+        expect(page).to have_no_link '削除', href:board_path(@board1)
+      end
+      # ツイート2に「'削除'」へのリンクがないことを確認する
+      visit board_path(@board2)
+      within '.more' do
+        expect(page).to have_no_link '削除', href:board_path(@board2)
+      end
     end
   end
 end
 
-RSpec.describe 'ツイート詳細', type: :system do
+RSpec.describe '掲示板詳細', type: :system do
   before do
-    @board = FactoryBot.create(:board)
+    @user = FactoryBot.create(:user)
+    @board1 = FactoryBot.create(:board, user: @user)
   end
-  it 'ログインしたユーザーはツイート詳細ページに遷移してコメント投稿欄が表示される' do
+  it 'ログインしたユーザーは掲示板詳細ページに遷移してコメント投稿欄が表示される' do
     # ログインする
     visit new_user_session_path
-    fill_in 'Email' , with:@board.user.email
-    fill_in 'Password' with:@board.user.password
+    fill_in 'メールアドレス' , with:@user.email
+    fill_in 'パスワード', with:@user.password
     find('input[name="commit"]').click
     expect(page).to have_current_path(authenticated_root_path)
-    # ツイートに「詳細」へのリンクがあることを確認する
-    expect(
-        all('.more')[1].hover
-      ).to have_link'show',href: tweet_path(@board)
+    # 掲示板1に「'この掲示板についてはこちら'」へのリンクがあることを確認する
+      within all('.board-list')[0]do
+        expect(page).to have_link 'この掲示板についてはこちら'
+      end
     # 詳細ページに遷移する
-    visit board_path(@board)
-    # 詳細ページにツイートの内容が含まれている
-    expect(page).to have_selector ".content_post[style='background-image: url(#{@board.title});']"
-    expect(page).to have_content("#{@board.description}")
+    visit board_path(@board1)
+    # 詳細ページに掲示板の内容が含まれている
+    # expect(page).to have_selector ".content_post[style='background-image: url(#{@board.title});']"
+    expect(page).to have_content("#{@board1.title}")
+    expect(page).to have_content("#{@board1.description}")
     # コメント用のフォームが存在する
     expect(page).to have_selector 'form'
   end
   it 'ログインしていない状態でツイート詳細ページに遷移できるもののコメント投稿欄が表示されない' do
     # トップページに移動する
     visit unauthenticated_root_path
-    # ツイートに「詳細」へのリンクがあることを確認する
-    expect(
-      all('.more')[0].hover
-    ).to have_link'show',href: board_path(@board)
+    # 掲示板に「'この掲示板についてはこちら'」へのリンクがあることを確認する
+      within all('.board-list')[0]do
+        expect(page).to have_link 'この掲示板についてはこちら'
+      end
     # 詳細ページに遷移する
-    visit board_path(@board)
-    # 詳細ページにツイートの内容が含まれている
-    expect(page).to have_selector "content_post[style='background-image: url(#{@board.title});']"
-    expect(page).to have_content("#{@board.description}")
+    visit board_path(@board1)
+    # 詳細ページに掲示板の内容が含まれている
+    expect(page).to have_content("#{@board1.title}")
+    expect(page).to have_content("#{@board1.description}")
     # フォームが存在しないことを確認する
     expect(page).to have_no_selector 'form'
     # 「コメントの投稿には新規登録/ログインが必要です」が表示されていることを確認する
