b4e3ac45be9f2b1564cf0f5ec509427d3f6b5d8c
Tomoki-Fukasawa <74344215+Tomoki-Fukasawa@users.noreply.github.com>
2026-02-19 22:38:08 +0900

    0219_friendship_test

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Friendship, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  it "同じ user と friend の組み合わせは重複できない" do
+    user = create(:user)
+    friend = create(:user)
+
+    create(:friendship, user: user, friend: friend)
+    duplicate = build(:friendship, user: user, friend: friend)
+
+    expect(duplicate).not_to be_valid
+  end
+
+  it "初期状態が pending である" do
+    friendship = create(:friendship)
+    expect(friendship.pending?).to be true
+  end
+  
+  it "accepted に変更できる" do
+    friendship = create(:friendship)
+    friendship.accepted!
+    expect(friendship.accepted?).to be true
+  end
 end
