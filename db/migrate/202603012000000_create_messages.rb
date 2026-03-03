81c89d65679379e2dcf2d516ab811d8c989164d4
Tomoki-Fukasawa <74344215+Tomoki-Fukasawa@users.noreply.github.com>
2026-03-04 06:47:53 +0900

    0303_chatroom_migrate

--- db/migrate/202603012000000_create_messages.rb
+++ db/migrate/202603012000000_create_messages.rb
@@ -2,7 +2,8 @@ class CreateMessages < ActiveRecord::Migration[7.1]
   def change
     create_table :messages do |t|
       t.references :user, null:false,foreign_key:true
-      t.references :friend, null:false,foreign_key: { to_table: :users }
+      # t.references :friend, null:false,foreign_key: { to_table: :users }
+      t.references :chat_room, null: false, foreign_key: true
       t.text :content
       t.timestamps
     end
