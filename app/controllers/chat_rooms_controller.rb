eda7111fb6d62420428e85913b01daa70c2b55b0
Tomoki-Fukasawa <74344215+Tomoki-Fukasawa@users.noreply.github.com>
2026-03-09 23:11:21 +0900

    0309_chatroom&message&test

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -1,26 +1,28 @@
 class ChatRoomsController < ApplicationController
+
   def index
     @chat_room=ChatRoom.new
     @chat_rooms=ChatRoom.where(user1_id: current_user.id).or(ChatRoom.where(user2_id: current_user.id))
-    @partner = partner(current_user)
+    @partner = @chat_room.partner(current_user)
   end
 
   def show
     @chat_room=ChatRoom.find(params[:id])
-    # if (current_user==@chat_room.user1) || (current_user==@chat_room.user2)
-    #   @messages=@chat_room.messages
-    #   @partner = @chat_room.partner(current_user)
-    # else
-    #   redirect_to root_path
-    # end
 
-    unless current_user.friends_with?(@chat_room.partner(current_user))
+    if (current_user==@chat_room.user1) || (current_user==@chat_room.user2)
+      @message=@chat_room.messages.new
+      @messages=@chat_room.messages.includes(:user)
+      @partner = @chat_room.partner(current_user)
+      unless current_user.friends_with?(@chat_room.partner(current_user))
+          redirect_to root_path
+        return
+      end
+    else
       redirect_to root_path
-      return
     end
-    @message=@chat_room.messages
-    @messages=@chat_room.messages.includes(:user)
-    @partner = @chat_room.partner(current_user)
+
+    
+    
   end
 
   # private
--- spec/factories/chat_rooms.rb
+++ spec/factories/chat_rooms.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :chat_room do
-    
+    name {Faker::Team.name}
   end
 end
