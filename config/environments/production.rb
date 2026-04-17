9cdd431b02dea226f7267ab63e49cbafec208694
Tomoki-Fukasawa <74344215+Tomoki-Fukasawa@users.noreply.github.com>
2026-04-17 19:30:06 +0900

    0417_ngrok_3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.hosts << ".ngrok-free.app"
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
