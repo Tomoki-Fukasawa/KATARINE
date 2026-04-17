b5f23e7e57d0de3d140d76ea98638a3a7349d2a9
Tomoki-Fukasawa <74344215+Tomoki-Fukasawa@users.noreply.github.com>
2026-04-17 20:58:24 +0900

    0417_ngrok_erase

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,16 +62,6 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
-  config.hosts << ".ngrok-free.dev"
-
-  config.action_mailer.default_url_options = {
-    host: "coroner-splicing-unmixed.ngrok-free.dev",
-    protocol: "https"
-  }
-  Rails.application.routes.default_url_options = {
-    host: "coroner-splicing-unmixed.ngrok-free.dev",
-    protocol: "https"
-  }
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  config.active_storage.service = :cloudinary
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
@@ -64,6 +64,10 @@ Rails.application.configure do
   # want to log everything, set the level to "debug".
   config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
 
+  config.action_mailer.default_url_options = {
+    host: "katarine.onrender.com",
+    protocol: "https"
+  }
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
